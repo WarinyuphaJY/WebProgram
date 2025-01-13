0bb0d5d285c535b1e983de64638685c9496b16d3
BlackBloodKCZ <147476701+WarinyuphaJY@users.noreply.github.com>
2025-01-13 18:11:30 +0700

    Add files via upload

---
+++ work/TEST/result.php
@@ -0,0 +1,35 @@
+<?php
+function add($n1, $n2) {
+    $result = $n1 + $n2;
+    echo "<br><br>Result add : " . $result;
+}
+
+function subtract($n1, $n2 = 50) {
+    $result = $n1 - $n2;
+    echo "<br><br>Result subtract : " . $result;
+}
+
+function multiply($n1, $n2) {
+    $result = $n1 * $n2;
+    return $result;
+}
+
+function divide($n1, $n2) {
+    $result = $n1 / $n2;
+    return $result;
+}
+
+$num1 = 10;
+$num2 = 20;
+
+$resultDivide = divide($num1, $num2);
+echo "<br>Result divide : " . $resultDivide;
+
+$resultMultiply = multiply($num1, $num2);
+echo "<br><br>Result Multiply : " . $resultMultiply;
+
+add($num1, $num2);
+subtract($num1);
+multiply($num1, $num2)
+
+?>
