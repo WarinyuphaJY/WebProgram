a5672b4027dacfb687f780b1981649967e71ccaf
BlackBloodKCZ <147476701+WarinyuphaJY@users.noreply.github.com>
2025-01-13 14:44:00 +0700

    Update lab8-2.php

--- 6606021610176/CH8/lab8-2.php
+++ 6606021610176/CH8/lab8-2.php
@@ -1,13 +1,16 @@
 <?php
+
 // declare function page_header
 function page_header() {
     echo '<html><head><title>ยินดีต้อนรับทุกท่าน</title><head>';
     echo '<body bgcolor="#ffffff">';
 }
-$user = "Somchai";
+
+$user = "Warinyupha";
 page_header();
 echo "สวัสดีคุณ $user";
 page_footer();
+
 // declare function page_footer
 function page_footer() {
     echo '<hr />ขอบคุณที่มาเยี่ยมชม';
